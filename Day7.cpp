f23c0c987c2c6d4b1bf9d9b055413080d957b759
Sajal Agrawal <sajal.agrawal1997@gmail.com>
2016-01-18 00:18:02 +0530

    Day7.cpp
    Arrays!

---
+++ Day7.cpp
@@ -0,0 +1,43 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+    int ar[n],i,j,t=0;
+    for(i=0;i<n;i++)
+        {
+        cin>>ar[i];
+    }
+ 
+     
+    for(i=n-1;i>=0;i--)
+        {
+        cout<<ar[i]<<" ";
+    }
+    
+    return 0;
+}
