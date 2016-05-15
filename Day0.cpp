99994fbc9f7b625f35ac5c91c1be004c4baa5d87
Sajal Agrawal <sajal.agrawal1997@gmail.com>
2016-05-15 12:54:39 +0530

    Update and rename Day0.c to Day0.cpp

---
+++ Day0.cpp
@@ -0,0 +1,22 @@
+//https://www.hackerrank.com/challenges/30-hello-world
+//Author - Sajal Agrawal
+//@sajalagrawal
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+   string inputString; // declare a variable to hold our input
+   getline(cin, inputString); // get a line of input from cin and save it to our variable
+  
+   // Your first line of output goes here
+   cout << "Hello, World." << endl;
+    cout<<inputString;
+   // Write the second line of output
+
+   return 0;
+}
