0e60f49c8b4727bfa24cae8a69736a5dcb1ae6cf
Sajal Agrawal <sajal.agrawal1997@gmail.com>
2016-09-01 01:37:30 +0530

    Update Day1.cpp

--- Day1.cpp
+++ Day1.cpp
@@ -1,22 +1,36 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
+//https://www.hackerrank.com/challenges/30-data-types
+//Author - Sajal Agrawal
+//@sajalagrawal
+
 #include <iostream>
-#include <algorithm>
+#include<iomanip>
+#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
 using namespace std;
 
+int main()
+{
+    int i=4;
+    double d=4.0;
+    string s="Hackerrank ";
+    // Declare second integer, double, and String variables.
+    int num;
+    double dou;
+    string str;
 
-int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
-    cout<<"Primitive : Double\n";
-cout<<"Primitive : Character\n";
-cout<<"Primitive : Boolean\n";
-cout<<"Primitive : Integer\n";
-cout<<"Referenced : String\n";
-cout<<"Primitive : Boolean\n";
-cout<<"Primitive : Double\n";
-cout<<"Primitive : Character\n";
-cout<<"Referenced : String\n";
-
+    // Read and save an integer, double, and String to your variables.
+    cin>>num>>dou;
+    getchar();
+    getline(cin,str);
+    // Print the sum of both integer variables on a new line.
+    cout<<(i+num)<<"\n";
+    // Print the sum of the double variables on a new line.
+    double a=dou+d;
+    cout<<setprecision(1)<<fixed<<a<<"\n";
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+    string an=s+str;
+    cout<<an;
     return 0;
 }
