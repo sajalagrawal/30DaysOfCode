5c73126eb73984817fcaa577f394aa24344b24d6
Sajal Agrawal <sajal.agrawal1997@gmail.com>
2016-10-16 20:42:50 +0530

    Update Day2.cpp

--- Day2.cpp
+++ Day2.cpp
@@ -1,3 +1,7 @@
+//https://www.hackerrank.com/challenges/30-operators
+//Author - Sajal Agrawal
+//@sajalagrawal
+
 #include <cmath>
 #include <cstdio>
 #include <vector>
@@ -8,27 +12,17 @@ using namespace std;
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
-   double m;
-int t;
-int x;
-float tip;
-
-float tax;
-
-float price;
-
-
-
-cin>>m;
-cin>>t;
-cin>>x;
-tip=(t*m)/100.0;
+    double m;
+    int t;
+    int x;
+    float tip,tax,price;
+    cin>>m;
+    cin>>t;
+    cin>>x;
+    tip=(t*m)/100.0;
     tax=(m*x)/100.0;
     price=m+tax+tip;
     
-cout<<"The final price of the meal is $"<<round(price)<<".";
-
-
-
+    cout<<"The total meal cost is "<<round(price)<<" dollars.";
     return 0;
 }
