a52f115392d8da41e58bf9b9c8805b40f1fb1c79
Sajal Agrawal <sajal.agrawal1997@gmail.com>
2016-12-26 13:37:10 +0530

    Update Day 25.cpp

--- Day 25.cpp
+++ Day 25.cpp
@@ -23,6 +23,18 @@ int main() {
 }
 
 bool isPrime(int n){
+    if(n==2)return true;
+    if(n==1 || n%2==0)return false;
+   
+    int i=3;
+    while(i*i<=n){
+        if(n%i==0)return false;
+        i=i+2;
+    }
+    return true;
+}
+/*
+bool isPrime(int n){
     if(n==1)return false;
     int i=2;
     while(i*i<=n){
@@ -30,4 +42,4 @@ bool isPrime(int n){
         i++;
     }
     return true;
-}
+}*/
