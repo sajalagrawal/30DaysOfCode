052e9a29e8e2b60f4f8e96f4d6d4d8f222ea87e9
Sajal Agrawal <sajal.agrawal1997@gmail.com>
2017-01-01 23:57:00 +0530

    Create Day 27.java

---
+++ Day 27.java
@@ -0,0 +1,29 @@
+//https://www.hackerrank.com/challenges/30-testing
+//Author - Sajal Agrawal
+//@sajal.agrawal1997@gmail.com
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("5");
+        
+        System.out.println("6 4");
+        System.out.println("-2 -1 0 3 2 6");
+        
+        System.out.println("5 2");
+        System.out.println("-1 0 2 3 5");
+        
+        System.out.println("7 3");
+        System.out.println("3 -1 0 2 3 5 6");
+        
+        System.out.println("3 2");
+        System.out.println("-1 0 1");
+        
+        System.out.println("4 3");
+        System.out.println("2 -1 0 4");
+    }
+}
